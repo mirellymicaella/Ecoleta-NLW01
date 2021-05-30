a853c7e773c3338419a05bff9873da8672aca65d
Mirelly_ <micaella.silva670@gmail.com>
2021-05-30 18:56:29 -0300

    The entire project

---
+++ server/src/database/conection.ts
@@ -0,0 +1,11 @@
+import knex from 'knex';
+import path from 'path';
+
+const conection= knex({
+    client:'sqlite3',
+    connection:{
+        filename: path.resolve(__dirname,'database.sqlite'),
+    },
+});
+
+export default conection;
